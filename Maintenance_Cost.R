baeb6a4ee21459c736174f76cd2cf6126d2ec2ee
brickett <brickett@users.noreply.github.com>
2018-09-28 10:34:33 -0500

    Full script

--- Maintenance_Cost.R
+++ Maintenance_Cost.R
@@ -13,12 +13,28 @@ library(tidyr)
 setwd("C:/Users/bjr21/Documents/GitHub/CMS_BoPM")
 
 # Load Data
-df <- read_excel("Leases.xlsx")
+df <- read_excel("CombinedDataFY14-18.xlsx")
 
 # Rename Data
-names(df)[1] <- "BldCode"
-names(df)[46] <- "Zip"
-names(df)[49] <- "NumPark"
-names(df)[50] <- "SqFt"
-names(df)[62] <- "UseCode"
-names(df)[77] <- "TotCost"
+names(df)[3] <- "FiscalYear"
+names(df)[10] <- "SqFt"
+names(df)[37] <- "TotCost"
+
+#subset
+df_active <- subset(df, df$TotCost!= 0)
+
+# Create Factor Variables
+df_active$Region.f <- factor(df_active$Region)
+
+# Run analysis
+CostModel <- summary(lm(TotCost ~ SqFt + Region.f + FiscalYear, data = df_active ))
+
+Tidy_CostModel <- tidy(CostModel)
+
+# Capture Residuals
+df_active_Clean <- df_active %>% drop_na(SqFt, Region.f, FiscalYear, TotCost)
+                                         
+df_active_Clean$Residuals <- CostModel$residuals
+
+#write to file
+write.csv(df_active_Clean, "Maintenance_residuals.csv")
