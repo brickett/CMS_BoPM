e383bb9153385e1a2204dc3bc3e363a065c9ef0c
brickett <brickett@users.noreply.github.com>
2018-09-28 16:08:00 -0500

    Extended analysis
    Split parking and building sqft, added an asbestos dummy

--- Maintenance_Cost.R
+++ Maintenance_Cost.R
@@ -3,11 +3,11 @@
 
 # Import Packages
 library(readxl)
-library(ggplot2)
 library(dplyr)
 library(magrittr)
 library(broom)
 library(tidyr)
+library(plm)
 
 #set directory to GitHub repo
 setwd("C:/Users/bjr21/Documents/GitHub/CMS_BoPM")
@@ -17,24 +17,34 @@ df <- read_excel("CombinedDataFY14-18.xlsx")
 
 # Rename Data
 names(df)[3] <- "FiscalYear"
-names(df)[10] <- "SqFt"
-names(df)[37] <- "TotCost"
+names(df)[10] <- "ParkSqFt"
+names(df)[11] <- "TotSqFt"
+names(df)[38] <- "TotCost"
+
+# Split Square Footage
+df$ParkSqFt[is.na(df$ParkSqFt)] <- 0
+df$BuildSqFt <- df$BuildSqFt <- df$TotSqFt - df$ParkSqFt
+
+# Convert Fiscal Year to Date
+df$FiscalYear.Date <- as.Date(paste(df$FiscalYear, 1, 1, sep = "-"))
 
 #subset
 df_active <- subset(df, df$TotCost!= 0)
+df_active <- df_active %>% drop_na(TotSqFt)
 
 # Create Factor Variables
 df_active$Region.f <- factor(df_active$Region)
+df_active$SiteFull.f <- factor(df_active$SiteFull)
+df_active$asbestosFlag <- not(is.na(df_active$`Asbestos Abatement`))
 
-# Run analysis
-CostModel <- summary(lm(TotCost ~ SqFt + Region.f + FiscalYear, data = df_active ))
+# Run analysis - Basic OLS
+CostModel_OLS <- summary(lm(TotCost ~ BuildSqFt + ParkSqFt + FiscalYear.Date + Region.f + asbestosFlag, data = df_active ))
 
-Tidy_CostModel <- tidy(CostModel)
+Tidy_CostModel_OLS <- tidy(CostModel_OLS)
 
 # Capture Residuals
-df_active_Clean <- df_active %>% drop_na(SqFt, Region.f, FiscalYear, TotCost)
-                                         
-df_active_Clean$Residuals <- CostModel$residuals
+df_active_Clean <- df_active %>% drop_na(TotSqFt, Region.f, FiscalYear.Date, TotCost)
+df_active_Clean$Residuals <- CostModel_OLS$OLSresiduals
 
 #write to file
 write.csv(df_active_Clean, "Maintenance_residuals.csv")
